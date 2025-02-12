1f97468894470080694912c37129dadae6264655
Vatsal Soni <sonivatsal3@gmail.com>
2025-02-12 12:11:06 +0530

    smtp production configs added

--- services/emailServices.js
+++ services/emailServices.js
@@ -7,10 +7,10 @@ const createTransporter = () => {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     secure: process.env.SMTP_SECURE === 'true',
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
+    // auth: {
+    //   user: process.env.SMTP_USERNAME,
+    //   pass: process.env.SMTP_PASSWORD,
+    // },
   });
 };
 
